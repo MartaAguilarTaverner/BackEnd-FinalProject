1680ae4bb5875af60f58cb5d64174e7f71108756
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2022-12-20 20:58:47 +0100

    Structure of the DB, general router(not routes), config, db(basic), index and the models not finished. Stucture and Geneal Elements

---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto-js');
+const fs = require('fs');
+const cors = require('cors');
+
+const logger = require('./config/winston');
+const db = require();
+const router = require('./router');
+
+const app = express();
+const PORT = process.env.PORT || 3006;
+const token = crypto.lib.WordArray.random(64).toString();
+const envFile = `TOKEN_SECRET =${token}`;
+
+fs.writeFileSync('./.env', envFile);
+
+dotenv.config();
+
+app.use(morgan('combined', { stream: logger.stream }));
+app.use(express.json());
+
+//cors
+app.use(router);
+
+//cors config
+db.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en el puerto ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
